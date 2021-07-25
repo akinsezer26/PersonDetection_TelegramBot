2f24263300674ab605e5840d1317ba02b59c6e02
akinsezer26 <38925265+akinsezer26@users.noreply.github.com>
2021-07-25 03:30:01 +0300

    update test

---
+++ update/TelegramBot.py
@@ -0,0 +1,307 @@
+from telegram.ext import Updater, CommandHandler
+import telegram
+import time
+import numpy as np
+import cv2
+from detector import yoloHandler
+from io import BytesIO
+from PIL import Image
+from subprocess import run
+from subprocess import PIPE
+import os
+import threading
+import datetime
+
+delay = 3.0
+isCalis = False
+ServerStartDate = datetime.datetime.now()
+interval = 0.5
+isDc = False
+dcStartH = 0
+dcStartM = 0
+dcEndH = 0
+dcEndM = 0
+
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
+def calis(update, context):
+    global isCalis
+    isCalis = True
+
+def calisma(update, context):
+    global isCalis
+    isCalis = False
+
+def getCalis(update, context):
+    duzC = ""
+    if(isDc == True):
+        duzC = "Acik\n"
+    else:
+        duzc = "Kapali\n"
+    if(isCalis==True):
+        str_ = "Calisiyor\n" + "Duzenli Calisma: " + duzC + str(dcStartH) +":"+ str(dcStartM) + " " + str(dcEndH) + ":" + str(dcEndM)
+        context.bot.send_message(chat_id=update.message.chat_id,text=str_)
+    else:
+        str_ = "Calismiyor\n" + "Duzenli Calisma: " + duzC + str(dcStartH) +":"+ str(dcStartM) + " " + str(dcEndH) + ":" + str(dcEndM)
+        context.bot.send_message(chat_id=update.message.chat_id,text=str_)
+
+def yardim(update, context):
+    str = "/sunucu_zamani\n"+"/calis\n"+"/calisma\n"+"/calisma_durumu\n"+"/sicaklik\n"+"/duzenli_calis\n"+"/duzenli_calisma\n"+"/duzenli_calismayi_ayarla\n"+"/gelismis\n"
+    context.bot.send_message(chat_id=update.message.chat_id,text=str)
+
+def developer(update, context):
+    str = "/manual\n"+"/update\n"+"/sunucu_zamani\n"+"/calis\n"+"/calisma\n"+"/calisma_durumu\n"+"/set_delay\n"+"/set_interval\n"+"/get_delay\n"+"/get_interval\n"+"/sicaklik\n"+"/yeniden_baslat\n"+"/kapat\n"+"/rapor\n"
+    context.bot.send_message(chat_id=update.message.chat_id,text=str)
+
+def getDelay(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,text=str(delay))
+
+def getInterval(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,text=str(interval))
+
+def setDc(update, context):
+    try:
+        global dcStartH
+        global dcStartM
+        global dcEndH
+        global dcEndM
+        stra =  update.message.text.split(' ')[1]
+        times = stra.split(':')
+
+        start = datetime.time(int(times[0]),int(times[1]),0)
+        end = datetime.time(int(times[2]),int(times[3]),0)
+
+        dcStartH = times[0]
+        dcStartM = times[1]
+        dcEndH   = times[2]
+        dcEndM   = times[3]
+        context.bot.send_message(chat_id=update.message.chat_id,text="Atama Başarılı")
+    except:
+        context.bot.send_message(chat_id=update.message.chat_id,text="Geçersiz Komut")
+
+def Dc(update, context):
+    global isDc
+    isDc = True
+
+def Dcn(update, context):
+    global isDc
+    global isCalis
+    isDc = False
+    isCalis = False
+
+def setDelay(update, context):
+    try:
+        global delay
+        temp = update.message.text.split(' ')[1]
+        temp = float(temp)
+        delay = temp
+        context.bot.send_message(chat_id=update.message.chat_id,text="delay "+str(delay)+"sn olarak atandı")
+    except:
+        context.bot.send_message(chat_id=update.message.chat_id,text="Geçersiz Komut")
+
+def setInterval(update, context):
+    try:
+        global interval
+        temp = update.message.text.split(' ')[1]
+        temp = float(temp)
+        interval = temp
+        context.bot.send_message(chat_id=update.message.chat_id,text="interval "+str(interval)+"sn olarak atandı")
+    except:
+        context.bot.send_message(chat_id=update.message.chat_id,text="Geçersiz Komut")
+
+def getServerTime(update, context):
+    elapsedtime = datetime.datetime.now() - ServerStartDate
+    elapsedtime_s = elapsedtime.total_seconds()
+    hours = divmod(elapsedtime_s, 3600)[0]
+    minutes = divmod(elapsedtime_s, 60)[0]%60
+    seconds = divmod(elapsedtime.seconds, 60)[1]
+
+    txt = str(round(hours))+' saat '+str(round(minutes))+' dakika '+str(round(seconds))+' saniye '
+    context.bot.send_message(chat_id=update.message.chat_id,text=txt)
+
+def getTemp(update, context):
+    output = run("sensors", stdout=PIPE, stderr=PIPE)
+    out = str(output)[55: -20: 1].replace(r'\xc2\xb0C','°C').replace(r'\n', '\n')
+    context.bot.send_message(chat_id=update.message.chat_id,text=out)
+
+import signal
+import psutil
+global_updater = None
+def shutdown(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,text="Sunucu 1 dk icinde kapanacaktır")
+    os.system("sudo shutdown -h +1")
+    global_updater.stop()
+
+def restart(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,text="Sunucu 1 dk icinde tekrar başlatılacaktır")
+    os.system("sudo shutdown -r +1")
+    global_updater.stop()
+
+def manual(update, context):
+    str = "delay    : Görüntü gönderme aralığı\ninterval : tespit etme aralığı"
+    context.bot.send_message(chat_id=update.message.chat_id,text=str)
+
+def server(bot, updater, ChatID):
+
+    global_updater = updater
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('sunucu_zamani', getServerTime))
+    dp.add_handler(CommandHandler('calis', calis))
+    dp.add_handler(CommandHandler('calisma', calisma))
+    dp.add_handler(CommandHandler('calisma_durumu', getCalis))
+    dp.add_handler(CommandHandler('yardim', yardim))
+    dp.add_handler(CommandHandler('gelismis', developer))
+    dp.add_handler(CommandHandler('set_delay', setDelay))
+    dp.add_handler(CommandHandler('set_interval', setInterval))
+    dp.add_handler(CommandHandler('get_delay', getDelay))
+    dp.add_handler(CommandHandler('get_interval', getInterval))
+    dp.add_handler(CommandHandler('sicaklik', getTemp))
+    dp.add_handler(CommandHandler('kapat',shutdown))
+    dp.add_handler(CommandHandler('yeniden_baslat',restart))
+    dp.add_handler(CommandHandler('manual',manual))
+    dp.add_handler(CommandHandler('rapor',report))
+    dp.add_handler(CommandHandler('duzenli_calismayi_ayarla',setDc))
+    dp.add_handler(CommandHandler('duzenli_calis',Dc))
+    dp.add_handler(CommandHandler('duzenli_calisma',Dcn))
+
+    updater.start_polling()
+
+    loop(bot, ChatID)
+
+    updater.idle()
+
+detectCount = 0
+totalError = 0
+totalBlackScreen = 0
+totalValid = 0
+totalPersonAvg = 0.0
+
+lis = [[] for i in range(25)]
+
+def report(update , context):
+    avg = 0.0
+    sum = 0.0
+    cnt = 0
+    for row in lis:
+        for element in row:
+            if(element[0] == b'person'):
+                cnt+=1
+                sum+=element[1]
+                ort=sum/cnt
+
+    dc = ""
+    if(detectCount<1000):
+        dc = str(detectCount)
+    elif(detectCount>1000 and detectCount<1000000):
+        dc = str(detectCount)[:-3]+"k"
+    else:
+        dc = str(detectCount)[:-6]+"m"
+
+    tv = ""
+    if(totalValid<1000):
+        tv = str(totalValid)
+    elif(totalValid>1000 and totalValid<1000000):
+        tv = str(totalValid)[:-3]+"k"
+    else:
+        tv = str(totalValid)[:-6]+"m"
+
+    te = ""
+    if(totalError<1000):
+        te = str(totalError)
+    elif(totalError>1000 and totalError<1000000):
+        te = str(totalError)[:-3]+"k"
+    else:
+        te = str(totalError)[:-6]+"m"
+
+    tbs = ""
+    if(totalBlackScreen<1000):
+        tbs = str(totalBlackScreen)
+    elif(totalBlackScreen>1000 and totalBlackScreen<1000000):
+        tbs = str(totalBlackScreen)[:-3]+"k"
+    else:
+        tbs = str(totalBlackScreen)[:-6]+"m"
+
+
+    result = "+========= Rapor ========+\n"
+    result +="Toplam Hesaplama= "+ dc+"\n"
+    result +="Toplam Tespit         = "+ tv+"\n"
+    result +="Toplam Hata            = "+ te +"\n"
+    result +="Topl Siyah Ekran      = "+ tbs+"\n"
+    result +="Ort İnsan Olasılık     = "+ "%"+str(totalPersonAvg*100)[:4]+"\n"
+    result +="Son 25 İ. Olasılık     = "+ "%"+str(avg*100)[:4]+"\n"
+    context.bot.send_message(chat_id=update.message.chat_id,text=result)
+
+def loop(bot, ChatID):
+
+    ch = yoloHandler(0)
+    time.sleep(5)
+    global lis
+    cnt = 0
+    global detectCount
+    global totalError
+    global totalBlackScreen
+    global totalValid
+    global totalPersonAvg
+    global isCalis
+
+    personCount = 0
+    personSum = 0.0
+    while(True):
+        dt = datetime.datetime.now()
+        dtH = dt.hour
+        dtM = dt.minute
+
+        start = datetime.time(int(dcStartH), int(dcStartM), 0)
+        end = datetime.time(int(dcEndH), int(dcEndM), 0)
+
+        if(isDc == True and time_in_range(start,end,datetime.time(dtH, dtM, 0)) ):
+            isCalis = True
+        elif(isDc == True and not time_in_range(start,end,datetime.time(dtH, dtM, 0))):
+            isCalis = False
+
+        if(isCalis == True):
+            result,output=ch.yolo_detect(float(interval))
+            if(cnt == 25):
+                cnt = 0
+            lis[cnt] = output
+            #print(str(lis[cnt])+"\n")
+
+            if(output == "Resim Siyah"):
+                totalBlackScreen+=1
+            elif(output == "Usb Takili Degil"):
+                totalError+=1
+            else:
+                totalValid+=1
+
+            currentProb = 0.0
+            for element in output:
+                if(element[0] == b'person'):
+                    personCount+=1
+                    currentProb = element[1]
+                    personSum+=element[1]
+                    totalPersonAvg=personSum/personCount
+
+            cnt+=1
+            detectCount+=1
+
+            if(result == True):
+                pilImage = Image.fromarray( cv2.cvtColor(ch.DetectedImage, cv2.COLOR_BGR2RGB) )
+                bio = BytesIO()
+                bio.name = 'image.jpeg'
+                pilImage.save(bio, 'JPEG')
+                bio.seek(0)
+
+                bot.send_photo(chat_id=ChatID, photo=bio, caption="Insan : %" + str(currentProb*100)[:4])
+                time.sleep(float(delay))
+        else:
+            time.sleep(1)
+
+if __name__ == '__main__':
+    server()
---
+++ update/detector.py
@@ -0,0 +1,82 @@
+import sys, os
+path = "/home/akin/Desktop/masaustu/darknet/"
+sys.path.insert(1, path + 'python')
+
+import darknet as dn
+import pdb
+import cv2
+import numpy as np
+import time
+import datetime
+
+class yoloHandler:
+
+    def __init__(self, source):
+        self.DetectedImage = None
+        dn.set_gpu(0)
+        self.net = dn.load_net(bytes(path + "cfg/yolov3-tiny.cfg", encoding='utf-8'), bytes(path + "/yolov3-tiny.weights", encoding='utf-8'), 0)
+        self.meta = dn.load_meta(bytes(path + "cfg/coco.data", encoding='utf-8'))
+        self.cap = cv2.VideoCapture(source)
+
+    def array_to_image(self, arr):
+        arr = arr.transpose(2,0,1)
+        c = arr.shape[0]
+        h = arr.shape[1]
+        w = arr.shape[2]
+        arr = (arr/255.0).flatten()
+        data = dn.c_array(dn.c_float, arr)
+        im = dn.IMAGE(w,h,c,data)
+        return im
+
+    def yolo_detect(self,interval):
+
+        time.sleep(interval)
+
+        try:
+            for i in range(24):
+                self.cap.grab()
+
+            ret,image = self.cap.read()
+
+            if(cv2.countNonZero(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))==0):
+                return False, "Resim Siyah"
+            else:
+                image_copy = image.copy()
+                cv2.rectangle(image_copy, (0,0), (50, 66), (0,0,0),-1)
+                cv2.rectangle(image_copy, (50,0), (100, 55), (0,0,0),-1)   #CAM1
+                cv2.rectangle(image_copy, (100,0), (150, 40), (0,0,0),-1)
+                cv2.rectangle(image_copy, (150,0), (210, 30), (0,0,0),-1)
+
+                cv2.rectangle(image_copy, (550,120), (600, 185), (0,0,0),-1)  #CAM2
+
+                image_dn = self.array_to_image(image_copy)
+                dn.rgbgr_image(image_dn)
+
+                hour = datetime.datetime.now().hour
+                dn.nnp_initialize()
+                r = None
+
+                #print("inceleniyor")
+
+                if(hour > 7 and hour < 18):
+                    r = dn.detect(self.net, self.meta, image_dn, 0.30)
+                else:
+                    r = dn.detect(self.net, self.meta, image_dn, 0.25)
+
+                length = len(r)
+
+                #print(r)
+
+                flag = False
+                if(length>0):
+                    self.DetectedImage = image
+                for i in range(length):
+                    if(str(r[i][0]).find("person") >= 0):
+                        self.DetectedImage = cv2.rectangle(self.DetectedImage, (int(r[i][2][0]) - int(r[i][2][2]) -5, int(r[i][2][1]) - int(r[i][2][3])-5), (int(r[i][2][0]) + int(r[i][2][2]) +5, int(r[i][2][1]) + int(r[i][2][3])+5), (255,0,255), 2)
+                        #print("Detected")
+                        flag = True
+
+                return flag, r
+        except:
+            #print("error")
+            return False, "Usb Takili Degil"
