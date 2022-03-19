0f584ad7c8b1c2ac7e26a95a899ead14fc6d21c5
akinsezer26 <38925265+akinsezer26@users.noreply.github.com>
2022-03-19 23:59:59 +0300

    Update TelegramBot.py

--- update/TelegramBot.py
+++ update/TelegramBot.py
@@ -242,10 +242,10 @@ daily_report_flag = 0
 def dailyReport(chat_id):
     now = datetime.datetime.now()
     if(now.hour == '0' and now.minute == '30' and daily_report_flag== 0):
-         bot.send_message(chat_id=update.message.chat_id, text='Sunucu Ayakta')
-         daily_report_flag = 1
-     else:
-         daily_report_flag = 0
+        bot.send_message(chat_id=update.message.chat_id, text='Sunucu Ayakta')
+        daily_report_flag = 1
+    else:
+        daily_report_flag = 0
 
 def loop(bot, ChatID):
 
